63400c77996174192a55d2072a2cfbacedb37f5c
Ing Adrián Salas <ing.s.adrian@gmail.com>
2023-09-03 03:41:25 -0500

    feat: :sparkles: Counter
    Add interfaz counter

--- REFLEX_PY/REFLEX_PY.py
+++ REFLEX_PY/REFLEX_PY.py
@@ -8,34 +8,36 @@ filename = f"{config.app_name}/{config.app_name}.py"
 
 
 class State(rx.State):
-    """The app state."""
+    count: int = 0
 
-    pass
+    def increment(self):
+        self.count += 1
+
+    def decrement(self):
+        self.count -= 1
+
 
 
 def index() -> rx.Component:
     return rx.fragment(
         rx.color_mode_button(rx.color_mode_icon(), float="right"),
         rx.vstack(
-            rx.heading("Bienvenido to Reflex!", font_size="2em"),
-            rx.box("Get started by editing ", rx.code(filename, font_size="1em")),
-            rx.link(
-                "Check out our docs!",
-                href=docs_url,
-                border="0.1em solid",
-                padding="0.5em",
-                border_radius="0.5em",
-                _hover={
-                    "color": rx.color_mode_cond(
-                        light="rgb(107,99,246)",
-                        dark="rgb(179, 175, 255)",
-                    )
-                },
+            rx.button(
+                "Decrement",
+                color_scheme="red",
+                border_radius="1em",
+                on_click=State.decrement,
             ),
-            spacing="1.5em",
+            rx.heading(State.count, font_size="2em"),
+            rx.button(
+                "Increment",
+                color_scheme="green",
+                border_radius="1em",
+                on_click=State.increment,
+            ),
             font_size="2em",
             padding_top="10%",
-        ),
+        )
     )
 
 
